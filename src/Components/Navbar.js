fb7f680654bf05782e044002faa5409c169f49c4
ashwantmanikoth <ashwanta75@gmail.com>
2024-04-08 13:04:53 -0300

    fix(app): Url change

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -123,7 +123,7 @@ function Navbar() {
 
           <MenuItem onClick={handleCloseMenu}>
             <a
-              href="https://evis-student.auth.us-east-1.amazoncognito.com/login?client_id=1e5ud4pjf6afm1j30s49apgiih&response_type=code&scope=aws.cognito.signin.user.admin+email+openid&redirect_uri=https%3A%2F%2F54.226.10.191%3A3000%2Fauth%2Fcallback"
+              href="https://evis-student.auth.us-east-1.amazoncognito.com/login?client_id=1e5ud4pjf6afm1j30s49apgiih&response_type=code&scope=aws.cognito.signin.user.admin+email+openid&redirect_uri=http%3A%2F%2F54.226.10.191%3A3000%2Fauth%2Fcallback"
               // href="https://evis-student.auth.us-east-1.amazoncognito.com/login?client_id=1e5ud4pjf6afm1j30s49apgiih&response_type=code&scope=aws.cognito.signin.user.admin+email+openid&redirect_uri=https%3A%2F%2Flocalhost%3A3000%2Fauth%2Fcallback"
               // href="https://evis-auth.auth.us-east-1.amazoncognito.com/oauth2/authorize?client_id=79jp9ca6spjm548a9o4pn2vs0f&response_type=code&scope=email+openid+phone&redirect_uri=https%3A%2F%2Flocalhost%3A3000%2Fauth%2Fcallback"
               rel="noopener noreferrer"
@@ -135,7 +135,7 @@ function Navbar() {
 
           <MenuItem onClick={handleCloseMenu}>
             <a
-              href="https://evis-professors.auth.us-east-1.amazoncognito.com/oauth2/authorize?client_id=7nsa0cqntm824rhqvo389r73eu&response_type=code&scope=aws.cognito.signin.user.admin+email+openid+phone&redirect_uri=https%3A%2F%2F54.226.10.191%3A3000%2Fauth%2Fcallback%2Fprofessor"
+              href="https://evis-professors.auth.us-east-1.amazoncognito.com/oauth2/authorize?client_id=7nsa0cqntm824rhqvo389r73eu&response_type=code&scope=aws.cognito.signin.user.admin+email+openid+phone&redirect_uri=http%3A%2F%2F54.226.10.191%3A3000%2Fauth%2Fcallback%2Fprofessor"
               // href="https://evis-professors.auth.us-east-1.amazoncognito.com/login?client_id=7nsa0cqntm824rhqvo389r73eu&response_type=code&scope=aws.cognito.signin.user.admin+email+openid+phone&redirect_uri=https%3A%2F%2Flocalhost%3A3000%2Fauth%2Fcallback%2Fprofessor"
               rel="noopener noreferrer"
               style={{ textDecoration: "none", color: "inherit" }}
